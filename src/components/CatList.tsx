98ecfe0ea7557126e1108b506698dbf61764f6f8
Hamin Park <haminjp@naver.com>
2022-04-06 00:01:42 +0900

    add CatItem

--- src/components/CatList.tsx
+++ src/components/CatList.tsx
@@ -3,18 +3,44 @@ import { RootStateOrAny, useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import styled from "styled-components";
 import * as searchActionCreators from "../modules/search";
+import defaultCatImage from "../img/defaultCat.jpeg";
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const BreedName = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: red;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(90, 0, 10, 0.4);
+  color: #fff;
+  font-weight: bold;
+  font-size: 1.5em;
+  transition: opacity 0.3s;
+  opacity: 0;
+`;
+
+const CatItem = styled.div`
+  position: relative;
+
+  &:hover ${BreedName} {
+    opacity: 1;
+  }
+`;
 
 const Container = styled.div`
   margin: 30px auto 0 auto;
   width: 1024px;
-  background-color: yellow;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-gap: 1em;
-`;
-
-const Item = styled.div`
-  background-color: green;
 `;
 
 const CatList = () => {
@@ -33,7 +59,13 @@ const CatList = () => {
   return (
     <Container>
       {catList.map((catInfo: any) => (
-        <Item key={catInfo.id}>{catInfo.name}</Item>
+        <CatItem key={catInfo.id}>
+          <Image
+            src={catInfo.image ? catInfo.image.url : defaultCatImage}
+            alt="cat"
+          />
+          <BreedName>{catInfo.name}</BreedName>
+        </CatItem>
       ))}
     </Container>
   );
