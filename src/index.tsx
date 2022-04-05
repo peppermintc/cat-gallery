c776b1325cff3a2df8f52ccc12a8ea58e58c4b0c
Hamin Park <haminjp@naver.com>
2022-04-05 16:43:17 +0900

    add styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import GlobalStyle from "./styles/GlobalStyle";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
