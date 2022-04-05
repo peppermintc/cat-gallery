db26598728f363dc9703e984c4e6be1a0ad6cd28
Hamin Park <haminjp@naver.com>
2022-04-05 16:59:20 +0900

    add component: Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  margin: 0.67em 0;
+`;
+
+const Header = () => {
+  return <Heading>😸 고양이 사진 갤러리 😻</Heading>;
+};
+
+export default Header;
