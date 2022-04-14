cd5b09fbd5b2f5b9b03356663911eeb8603e8a53
Hamin Park <haminjp@naver.com>
2022-04-14 18:36:12 +0900

    useMemo on actionCreators

--- src/hooks/useActionCreators.ts
+++ src/hooks/useActionCreators.ts
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as searchActionCreators from "../modules/cat";
@@ -5,9 +6,9 @@ import * as searchActionCreators from "../modules/cat";
 const useActionCreators = () => {
   const dispatch = useDispatch();
 
-  const { setCatList, setAutoComplete } = bindActionCreators(
-    searchActionCreators,
-    dispatch
+  const { setCatList, setAutoComplete } = useMemo(
+    () => bindActionCreators(searchActionCreators, dispatch),
+    [dispatch]
   );
 
   return {
